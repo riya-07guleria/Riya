ef189c30287a2d3e240c5f96675833122510460d
riya-07guleria <guleriariya917@gmail.com>
2025-07-16 15:48:49 +0530

    water bottle

---
+++ Bottle consumption.py
@@ -0,0 +1,13 @@
+total_bottles =10
+daily_consumption =3
+day=1
+while total_bottles > 0:
+    if total_bottles >=
+    daily_consumption:
+    drank= daily_consumption
+else:
+    drank = total_bottles
+    total_bottles -= drank
+    print(f"Day {day}:Drank {Drank} bottles(s). {total_bottles} left.")
+day += 1
+print("# No more bottles left!")
